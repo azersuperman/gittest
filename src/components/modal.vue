7aa987f88d3ad4696f3d02054683181183731d5c
zhangkx <1099253732@qq.com>
2024-06-24 15:12:47 +0800

    共同完善

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="modal">
-        <h2>当前求和为{{ num }}</h2>
+        <h2>{{ content }}</h2>
+        <div>求和为{{ num }}</div>
     </div>
 </template>
 <script setup name="Modal">
 import axios from "axios";
+import { ref } from "vue";
 let {
     data: { content },
 } = await axios.get("http://api.uomg.com/api/rand.qinghua?format=json");
 console.log(content);
+// b
+let num = ref(8);
+
+// a
 </script>
 <style scoped>
 .modal {
